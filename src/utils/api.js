5befb39ec5cdb1abc256c94733101e61836f0e04
mailmesapna1 <mailmesapna1@gmail.com>
2023-12-31 15:54:43 +0530

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": "57ce4c138dmsh128d29e4120557dp1c357ejsn83c555ec494b",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
